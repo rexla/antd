913f50bc1ac896ef97310828031352917f75ce12
rexla <45387759+rexla@users.noreply.github.com>
2022-06-23 17:43:31 +0800

    wow

--- pages/index.js
+++ pages/index.js
@@ -17,12 +17,27 @@ import {
   AspectRatio,
   Select,
   CheckIcon,
-  BottomSheet,
 } from "native-base";
-import City from "../components/city";
 
 // Start editing here, save and see your changes.
 export default function App() {
+  const Example = () => {
+    let [service, setService] = React.useState("");
+    return <Center>
+        <Box w="3/4" maxW="300">
+          <Select selectedValue={service} minWidth="200" accessibilityLabel="Choose Service" placeholder="Choose Service" _selectedItem={{
+          bg: "teal.600",
+          endIcon: <CheckIcon size="5" />
+        }} mt={1} onValueChange={itemValue => setService(itemValue)}>
+            <Select.Item label="UX Research" value="ux" />
+            <Select.Item label="Web Development" value="web" />
+            <Select.Item label="Cross Platform Development" value="cross" />
+            <Select.Item label="UI Designing" value="ui" />
+            <Select.Item label="Backend Development" value="backend" />
+          </Select>
+        </Box>
+      </Center>;
+  };
   return (
     <Center
       flex={1}
@@ -113,28 +128,3 @@ function ColorModeSwitch() {
     </Tooltip>
   );
 }
-
-const Example = () => {
-  let [service, setService] = React.useState("");
-  return <Center>
-      <Box w="3/4" maxW="300">
-        <Select 
-        selectedValue={service} 
-        minWidth="200" 
-        accessibilityLabel="Choose Service" 
-        placeholder="Choose Service" 
-        _selectedItem={{
-        bg: "teal.600",
-        endIcon: <CheckIcon size="5" />
-      }} 
-      mt={1} 
-      onValueChange={itemValue => setService(itemValue)}>
-          <Select.Item label="UX Research" value="ux" />
-          <Select.Item label="Web Development" value="web" />
-          <Select.Item label="Cross Platform Development" value="cross" />
-          <Select.Item label="UI Designing" value="ui" />
-          <Select.Item label="Backend Development" value="backend" />
-        </Select>
-      </Box>
-    </Center>;
-};
